5c546ec633f36a332cf30e64d182d52a09b05d8f
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-01-01 16:06:29 -0500

    setup for pip installation

---
+++ frontier_exploration/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="frontier_exploration",
+    version="0.1",
+    author="Naoki Yokoyama",
+    author_email="naokiyokoyama@github",
+    description="Frontier_exploration for habitat",
+    url="https://github.com/naokiyokoyama/frontier_exploration",
+    packages=setuptools.find_packages(),
+)
