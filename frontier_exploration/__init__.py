2a9797f43f24cbec22090eaf0e4ed6c58b10d1a3
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-01-03 08:21:02 -0500

    moved files to proper locations, oops

---
+++ frontier_exploration/__init__.py
@@ -0,0 +1,2 @@
+import frontier_exploration1.explorer  # necessary for updating registry!!
+import frontier_exploration1.measurements  # necessary for updating registry!!
--- frontier_exploration/frontier_exploration/__init__.py
+++
@@ -1,2 +0,0 @@
-import frontier_exploration.explorer  # necessary for updating registry!!
-import frontier_exploration.measurements  # necessary for updating registry!!
