56857aa583f8549211937aa274f94583c65bf5ab
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-01-08 17:30:04 -0500

    using numba for speed and using new formulas for finding frontier midpoints / closest points

--- frontier_exploration/explorer.py
+++ frontier_exploration/explorer.py
@@ -1,28 +1,31 @@
-from typing import Optional
+from typing import List, Optional
 
 import cv2
 import numpy as np
+from numba import njit
+
+from frontier_exploration.utils import closest_line_segment
 
 
 def detect_frontier_waypoints(
-    full_map: np.ndarray, explored_map: np.ndarray, area_thresh: Optional[int] = -1
+    full_map: np.ndarray, explored_mask: np.ndarray, area_thresh: Optional[int] = -1
 ):
-    mask = detect_frontiers(full_map, explored_map, area_thresh)
-    return frontier_waypoints(mask)
+    frontiers = detect_frontiers(full_map, explored_mask, area_thresh)
+    waypoints = frontier_waypoints(frontiers)
+    return waypoints
 
 
 def detect_frontiers(
     full_map: np.ndarray,
-    explored_map: np.ndarray,
+    explored_mask: np.ndarray,
     area_thresh: Optional[int] = -1,
-    whiten: Optional[bool] = True,
-) -> np.ndarray:
+) -> List[np.ndarray]:
     """Detects frontiers in a map.
 
     Args:
         full_map (np.ndarray): White polygon on black image, where white is navigable.
         Mono-channel mask.
-        explored_map (np.ndarray): Portion of white polygon that has been seen already.
+        explored_mask (np.ndarray): Portion of white polygon that has been seen already.
         This is also a mono-channel mask.
         area_thresh (int, optional): Minimum unexplored area (in pixels) needed adjacent
         to a frontier for that frontier to be valid. Defaults to -1.
@@ -30,89 +33,122 @@ def detect_frontiers(
     Returns:
         np.ndarray: A mono-channel mask where white contours represent each frontier.
     """
-    if whiten:
-        use_full_map, use_explored_map = [
-            np.where(i > 0, 255, i) for i in (full_map, explored_map)
-        ]
-    else:
-        use_full_map, use_explored_map = full_map, explored_map
-    if area_thresh != -1:
-        use_explored_map = filter_out_small_unexplored(
-            use_full_map, use_explored_map, area_thresh
-        )
-
-    # Blur the full_map with a kernel of 3
-    blurred_map = cv2.blur(use_explored_map, (3, 3))
-    # Threshold the blurred map to get a binary image
-    _, blurred_map = cv2.threshold(blurred_map, 0, 255, cv2.THRESH_BINARY)
-    # Make a three channel image by stacking all images
-    stacks = np.stack((use_full_map, use_explored_map, blurred_map), axis=2)
-    # Make a single-channel mask where pixels are white if they are:
-    # 1). navigable (white in full_map);
-    # 2). unexplored (black in explored_map), but also;
-    # 3). adjacent to explored pixels (white in blurred_map)
-    mask = np.all(stacks == [255, 0, 255], axis=2).astype(np.uint8) * 255
-
-    return mask
+    # Find the contour of the explored area
+    contours, _ = cv2.findContours(
+        filter_out_small_unexplored(full_map, explored_mask, area_thresh),
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE,
+    )
+    unexplored_mask = np.where(explored_mask > 0, 0, full_map)
+    unexplored_mask = cv2.blur(  # blurring for some leeway
+        np.where(unexplored_mask > 0, 255, unexplored_mask), (3, 3)
+    )
+    frontiers = []
+    # TODO: There shouldn't be more than one contour (only one explored area on map)
+    for contour in contours:
+        frontiers.extend(contour_to_frontiers(contour, unexplored_mask))
+    return frontiers
 
 
 def filter_out_small_unexplored(
-    full_map: np.ndarray, explored_map: np.ndarray, area_thresh: int
+    full_map: np.ndarray, explored_mask: np.ndarray, area_thresh: int
 ):
     """Edit the explored map to add small unexplored areas, which ignores their
     frontiers."""
-    # Make mask of unexplored areas
-    stacks = np.stack((full_map, explored_map), axis=2)
-    unexplored_mask = np.all(stacks == [255, 0], axis=2).astype(np.uint8) * 255
+    if area_thresh == -1:
+        return explored_mask
+    unexplored_mask = np.where(explored_mask > 0, 0, full_map)
     # Find contours in the unexplored mask
     contours, _ = cv2.findContours(
         unexplored_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
     # Add small unexplored areas to the explored map
-    new_explored_map = explored_map.copy()
+    new_explored_mask = explored_mask.copy()
     for contour in contours:
         if cv2.contourArea(contour) < area_thresh:
-            cv2.drawContours(new_explored_map, [contour], -1, 255, -1)
-    return new_explored_map
+            cv2.drawContours(new_explored_mask, [contour], -1, 255, -1)
+    return new_explored_mask
 
 
-def frontier_waypoints(frontier_mask: np.ndarray) -> np.ndarray:
-    """Finds waypoints in a frontier mask.
+@njit
+def contour_to_frontiers(contour, unexplored_mask):
+    """Given a contour from OpenCV, return a list of numpy arrays. Each array contains
+    contiguous points forming a single frontier. The contour is assumed to be a set of
+    contiguous points, but some of these points are not on any frontier, indicated by a
+    value of 0 in the unexplored mask. This function will split the contour into
+    multiple arrays"""
+    bad_inds = []
+    for idx in range(len(contour)):
+        point = contour[idx][0]
+        x, y = point
+        if unexplored_mask[y, x] == 0:
+            bad_inds.append(idx)
+    frontiers = np.split(contour, bad_inds)
+    frontiers = [i for i in frontiers if len(i) > 1]
+    return frontiers
+
+
+def frontier_waypoints(
+    frontiers: List[np.ndarray], xy: Optional[np.ndarray] = None
+) -> np.ndarray:
+    """For each given frontier, returns the point on the frontier closest (euclidean
+    distance) to the given coordinate. If coordinate is not given, will just return
+    the midpoints of each frontier.
 
     Args:
-        frontier_mask (np.ndarray): A mono-channel mask where white contours represent
-        each frontier.
+        frontiers (List[np.ndarray]): list of arrays of shape (X, 1, 2), where each
+        array is a frontier and X is NOT the same across arrays
+        xy (np.ndarray): the given coordinate
 
     Returns:
-        np.ndarray: An array of waypoints, where each waypoint is an array of (x, y)
+        np.ndarray: array of waypoints, one for each frontier
     """
-    # Find contours in the frontier mask
-    contours, _ = cv2.findContours(
-        frontier_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    if xy is None:
+        return np.array([get_frontier_midpoint(i) for i in frontiers])
+    return np.array([get_closest_frontier_point(xy, i) for i in frontiers])
+
+
+@njit
+def get_frontier_midpoint(frontier) -> np.ndarray:
+    """Given a list of contiguous points (numpy arrays) representing a frontier, first
+    calculate the total length of the frontier, then find the midpoint of the
+    frontier"""
+    # First, reshape and expand the frontier to be a 2D array of shape (X, 2)
+    # representing line segments between adjacent points
+    line_segments = np.concatenate((frontier[:-1], frontier[1:]), axis=1).reshape(
+        (-1, 2, 2)
     )
-    return np.array([find_center_of_contour(contour) for contour in contours])
+    # Calculate the length of each line segment
+    line_lengths = np.sqrt(
+        np.square(line_segments[:, 0, 0] - line_segments[:, 1, 0])
+        + np.square(line_segments[:, 0, 1] - line_segments[:, 1, 1])
+    )
+    cum_sum = np.cumsum(line_lengths)
+    total_length = cum_sum[-1]
+    # Find the midpoint of the frontier
+    midpoint = total_length / 2
+    # Find the line segment that contains the midpoint
+    line_segment_idx = np.argmax(cum_sum > midpoint)
+    # Calculate the coordinates of the midpoint
+    line_segment = line_segments[line_segment_idx]
+    line_length = line_lengths[line_segment_idx]
+    # Use the difference between the midpoint length and cumsum
+    # to find the proportion of the line segment that the midpoint is at
+    proportion = (midpoint - cum_sum[line_segment_idx - 1]) / line_length
+    # Calculate the midpoint coordinates
+    midpoint = line_segment[0] + proportion * (line_segment[1] - line_segment[0])
+    return midpoint
 
 
-def find_center_of_contour(contour):
-    moments = cv2.moments(contour)
-    center_x = int(moments["m10"] / moments["m00"])
-    center_y = int(moments["m01"] / moments["m00"])
-
-    # Check if the center point is within the contour
-    if cv2.pointPolygonTest(contour, (center_x, center_y), False) >= 0:
-        return center_x, center_y
-
-    # Initialize variables to store the nearest point and minimum distance
-    nearest_point = None
-    min_distance = float("inf")
-
-    for point in contour:
-        # Find the distance between the center point and the current point
-        distance = ((center_x - point[0][0]) ** 2) + ((center_y - point[0][1]) ** 2)
-        if distance < min_distance:
-            min_distance = distance
-            nearest_point = point[0]
-    return nearest_point
+def get_closest_frontier_point(xy, frontier):
+    """Returns the point on the frontier closest to the given coordinate."""
+    # First, reshape and expand the frontier to be a 2D array of shape (X, 2)
+    # representing line segments between adjacent points
+    line_segments = np.concatenate([frontier[:-1], frontier[1:]], axis=1).reshape(
+        (-1, 2, 2)
+    )
+    closest_segment, closest_point = closest_line_segment(xy, line_segments)
+    return closest_point
 
 
 if __name__ == "__main__":
@@ -127,9 +163,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "-e",
-        "--explored_map",
+        "--explored_mask",
         help="Path to explored map image",
-        default="explored_map.png",
+        default="explored_mask.png",
     )
     parser.add_argument(
         "-a",
@@ -144,19 +180,19 @@ if __name__ == "__main__":
     # Read in the map
     full_map = cv2.imread(args.full_map, 0)
     # Read in the explored map
-    explored_map = cv2.imread(args.explored_map, 0)
-    start_time = time.time()
-    # Detect the frontiers
-    frontier_mask = detect_frontiers(full_map, explored_map, args.area_thresh)
-    # Find the waypoints
-    waypoints = frontier_waypoints(frontier_mask)
-    print("Time taken for algorithm:", time.time() - start_time)
+    explored_mask = cv2.imread(args.explored_mask, 0)
+    times = []
+    for _ in range(501):
+        start_time = time.time()
+        waypoints = detect_frontier_waypoints(full_map, explored_mask, args.area_thresh)
+        times.append(time.time() - start_time)
+    # Skip first run as it's slower
+    print("Avg. time taken for algorithm:", np.mean(times[1:]))
 
     # Plot the results
     plt.figure(figsize=(10, 10))
     plt.imshow(full_map, cmap="gray")
-    plt.imshow(explored_map, cmap="gray", alpha=0.5)
-    plt.imshow(frontier_mask, cmap="gray", alpha=0.5)
+    plt.imshow(explored_mask, cmap="gray", alpha=0.5)
     for waypoint in waypoints:
         plt.scatter(waypoint[0], waypoint[1], c="red", s=50)
     plt.show()
