42e1a1a53c94c89edafd4a0afe473471451740a5
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2023-01-09 17:51:12 -0500

    allowing habitat install to be optional for easier local testing

--- frontier_exploration/__init__.py
+++ frontier_exploration/__init__.py
@@ -1,6 +1,18 @@
 """The following imports are necessary for updating the registry"""
-import frontier_exploration.explorer
-import frontier_exploration.measurements
-import frontier_exploration.policy
-import frontier_exploration.sensors
-import frontier_exploration.trainer
+try:
+    import frontier_exploration.explorer
+    import frontier_exploration.measurements
+    import frontier_exploration.policy
+    import frontier_exploration.sensors
+    import frontier_exploration.trainer
+except ModuleNotFoundError as e:
+    # If the error was due to the habitat package not being installed, then pass, but
+    # print a warning. Do not pass if it was due to another package being missing.
+    if e.name != "habitat":
+        raise e
+    else:
+        print(
+            "Warning: habitat package not installed. Cannot register habitat_baselines "
+            "components."
+        )
+
